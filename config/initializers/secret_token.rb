e81bb9725302efa7d5c2e44110f87784673e6774
mostlyclicks <carlos@mostlyclicks.com>
2012-10-10 22:40:41 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FbThree::Application.config.secret_token = 'f282a21e9c23110552e66eabb9d045aab6fb7fea67cba482eadf6ad1ffd3a12f60c61904f9ca0d8d347ceddb0de79d1acfbacc5b67b2b7204f67f4f9d92cb889'
